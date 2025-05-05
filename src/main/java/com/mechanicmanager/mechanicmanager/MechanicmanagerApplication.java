3ca27f528dfe2a664175c7b3884e384ef3a5e42f
Mota01G <gabrielfillipe92@gmail.com>
2025-05-05 19:44:57 -0300

    strutura do Projeto Gerada pelo Spring Initializr

---
+++ src/main/java/com/mechanicmanager/mechanicmanager/MechanicmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mechanicmanager.mechanicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mechanicmanager/mechanicmanager/MechanicmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechanicmanager.mechanicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
